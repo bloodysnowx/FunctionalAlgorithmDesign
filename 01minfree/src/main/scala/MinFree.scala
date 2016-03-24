ab341efba4c1ba8edbb6eb628603d5d2b9f24eda
bloodysnowx <bloodysnowx@gmail.com>
2016-03-24 09:29:17 +0900

    wip

---
+++ 01minfree/src/main/scala/MinFree.scala
@@ -0,0 +1,4 @@
+object MinFree extends App {
+    println(searchMinFree(Seq()))
+    def searchMinFree(ary: Seq[Int]): Int = 0
+}
---
+++ 01minfree/src/test/scala/MinFreeSpec.scala
@@ -0,0 +1,7 @@
+import org.scalatest._
+
+class MinFreeSpec extends FlatSpec with Matchers {
+  "MinFree" should "have tests" in {
+    true should be === true
+  }
+}
